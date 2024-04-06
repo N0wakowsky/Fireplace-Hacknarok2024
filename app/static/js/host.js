054e9a5a1e8dd5940293bb83f1aba0ca55cb9823
Kacper Pelczarski <kacperpelczarski11@gmail.com>
2024-04-06 18:03:45 +0200

    Dołączanie do ognisk

---
+++ app/static/js/host.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var Button = document.querySelector('.button');
+    Button.addEventListener('click', function() {
+        window.location.href = "/host";
+    });
+});
