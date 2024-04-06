78728fe2011642eb655e2b9729625ccb7eff5a70
Kacper Pelczarski <kacperpelczarski11@gmail.com>
2024-04-06 14:22:02 +0200

    Interface for login

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
     return render_template("join.html")
+
+@app.route("/main")
+def main():
+    return f"GOwno1"
